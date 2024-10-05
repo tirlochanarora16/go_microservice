06877d3346721011c4fbb2a68b9df8fba7a16a32
tirlochan016 <trilochan@crashnova.com>
2024-10-05 20:04:49 +0530

    graceful shutdown implemented

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/tirlochanarora16/go_microservice/application"
 )
@@ -10,7 +12,11 @@ import (
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+
+	defer cancel()
+
+	err := app.Start(ctx)
 
 	if err != nil {
 		fmt.Println("failed to start app:", err)
