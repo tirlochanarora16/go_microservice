78a6703da947d0cbb6a64eb75fb06dc558956b8d
tirlochan016 <trilochan@crashnova.com>
2024-10-08 22:51:24 +0530

    successfully integrated the insert method

--- application/routes.go
+++ application/routes.go
@@ -6,9 +6,10 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/tirlochanarora16/go_microservice/handler"
+	"github.com/tirlochanarora16/go_microservice/repository/order"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
@@ -16,13 +17,17 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	router.Route("/orders", loadOrderRoutes)
+	router.Route("/orders", a.loadOrderRoutes)
 
-	return router
+	a.router = router
 }
 
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
+func (a *App) loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.List)
--- handler/order.go
+++ handler/order.go
@@ -1,14 +1,60 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/tirlochanarora16/go_microservice/model"
+	"github.com/tirlochanarora16/go_microservice/repository/order"
 )
 
-type Order struct{}
+type Order struct {
+	Repo *order.RedisRepo
+}
 
-func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create order")
+func (h *Order) Create(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		CustomerID uuid.UUID        `json:"customer_id"`
+		LineItems  []model.LineItem `json:"line_items"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	now := time.Now().UTC()
+
+	order := model.Order{
+		OrderID:    rand.Uint64(),
+		LineItems:  body.LineItems,
+		CustomerID: body.CustomerID,
+		CreatedAt:  &now,
+	}
+
+	err := h.Repo.Insert(r.Context(), order)
+
+	if err != nil {
+		fmt.Println("failed to insert: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res, err := json.Marshal(order)
+
+	if err != nil {
+		fmt.Println("failed to marshal:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(res)
+	w.WriteHeader(http.StatusCreated)
 }
 
 func (o *Order) List(w http.ResponseWriter, r *http.Request) {
