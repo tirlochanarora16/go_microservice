e9a5ebeeea79930d8983511341d8a6dfacab870e
tirlochan016 <trilochan@crashnova.com>
2024-10-09 20:07:40 +0530

    added a bit of minor configuration

--- application/app.go
+++ application/app.go
@@ -12,11 +12,15 @@ import (
 type App struct {
 	router http.Handler
 	rdb    *redis.Client
+	config Config
 }
 
-func New() *App {
+func New(config Config) *App {
 	app := &App{
-		rdb: redis.NewClient(&redis.Options{}),
+		rdb: redis.NewClient(&redis.Options{
+			Addr: config.RedisAddress,
+		}),
+		config: config,
 	}
 
 	app.loadRoutes()
@@ -26,7 +30,7 @@ func New() *App {
 
 func (a *App) Start(ctx context.Context) error {
 	server := &http.Server{
-		Addr:    ":3000",
+		Addr:    fmt.Sprintf(":%s", a.config.ServerPort),
 		Handler: a.router,
 	}
 
